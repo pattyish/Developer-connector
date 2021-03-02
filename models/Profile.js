f20e701f57bad5f86da6303ba3c56baf33277366
pattyish <patrickishimwe10@gmail.com>
2021-03-02 23:28:34 +0200

    get logged in user info endpoint

--- models/Profile.js
+++ models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
   user: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'users',
   },
   handle: {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/Auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route  api/Profile
+// @route  api/Profile/me
 // @desc   Test route
-// @access Public 
-router.get('/', (req, res) => {
-    res.send('Testing apis');
-  });
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({
+        msg: 'There is no profile for this user!!!',
+      });
+    }
+    res.status(200).json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ msg: 'Server Error!!' });
+  }
+});
 
 module.exports = router;
