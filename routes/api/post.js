d1aaa21d7032609c4f725334cdda5cbca108dbf7
pattyish <patrickishimwe10@gmail.com>
2021-03-04 21:36:13 +0200

    creating post endpoint

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/Auth');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
-// @route  api/Posts
-// @desc   Test route
-// @access Public 
-router.get('/', (req, res) => {
-    res.send('Testing apis');
-  });
+// @route  api/posts
+// @desc   Create Post
+// @access Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required!!').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const text = req.body.text;
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        text: text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.status(201).json({
+        msg: 'Post created successfull!!',
+        post,
+      });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: 'Server Error!!' });
+    }
+  },
+);
 
 module.exports = router;
