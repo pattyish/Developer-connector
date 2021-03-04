1d6943af07bb440adf39f48343696b0e4f05c151
pattyish <patrickishimwe10@gmail.com>
2021-03-04 12:50:44 +0200

    delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,4 +194,29 @@ router.put(
     }
   },
 );
+// @route  Delete api/Profile/experience/:exp_id
+// @desc   Delete profile experience
+// @access
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.status(201).json({
+      msg: 'Experience Removed Successful!!!',
+      profile,
+    });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res
+        .status(400)
+        .json({ msg: 'Experience You are trying to Delete not found!!' });
+    }
+    res.status(500).json({ msg: 'Server Error!!' });
+  }
+});
 module.exports = router;
